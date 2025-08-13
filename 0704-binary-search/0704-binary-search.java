18c40a79e3f53a58ace25187ebf755f92d0805e0
Somadevaa <144410439+Somadevaa@users.noreply.github.com>
2025-08-13 14:27:37 +0530

    Time: 0 ms (100%), Space: 45.9 MB (33.82%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] a, int target) {
+        int l=0;
+        int r=a.length-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(a[mid]==target)
+            return mid;
+            else if(a[mid]>target){
+                r=mid-1;
+            }
+            else if(a[mid]<target){
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
+}
