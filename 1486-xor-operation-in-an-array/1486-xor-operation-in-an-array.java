6402331c8a35b5371215c54a41c5aa2fee42f829
Somadevaa <144410439+Somadevaa@users.noreply.github.com>
2026-01-30 17:46:20 +0530

    Time: 0 ms (100%), Space: 42 MB (84.48%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int [] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=start;
+            start+=2;
+
+        }
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum^arr[i];
+
+        }
+        return sum;
+        
+    }
+}
