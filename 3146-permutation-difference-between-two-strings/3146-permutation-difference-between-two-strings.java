7bb400f127ca456e936303e9b341734e5cb4d151
Somadevaa <144410439+Somadevaa@users.noreply.github.com>
2026-02-18 20:15:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findPermutationDifference(String s, String t) {
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+           sum+= Math.abs(s.charAt(i)-t.charAt(i));
+
+
+        }
+        return sum;
+        
+    }
+}
